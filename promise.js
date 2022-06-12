08f98fdcd1a3c79249ad95d29368c68d1b9911ec
Psianturi <posma2018@gmail.com>
2022-06-12 20:58:57 +0700

    Ini update challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  let paramFungsi = (
+    await promiseTheaterIXX()).concat((await promiseTheaterVGC())
+    );
+
+  return paramFungsi.filter(
+    data => (data.hasil === emotion)).length;
+};
 
 module.exports = {
   promiseOutput,
